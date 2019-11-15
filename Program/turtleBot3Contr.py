59b3666d7eedfeec3ff22ae058e6041a54079319
PlentyBugs <43182741+PlentyBugs@users.noreply.github.com>
2019-11-15 21:31:46 +0300

    Some content

---
+++ Program/turtleBot3Contr.py
@@ -0,0 +1,33 @@
+import rospy
+from geomentry_msgs.msgs import Twist
+from sensor_msgs.msg import LaserScan
+
+
+def talker():
+	rospy.init_node('vel_publisher')
+	pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+	move = Twist()
+	rate = rospy.Rate(1)
+	while not rospy.is_shutdown:
+		move.linear.x = 1
+		move.angular.z = 1
+		pub.publish(move)
+		rate.sleep()
+
+
+def callback(msg):
+	print("=========================") #From 0 to 359
+	print('Front: ' + msg.ranges(0))
+	print('Left: ' + msg.ranges(90))
+	print('Back: ' + msg.ranges(180))
+	print('Right: ' + msg.ranges(270))
+
+
+if __name__ == '__main__':
+	try:
+		rospy.init_node('laser_data')
+		sub = rospy.Subscriber('scan', LaserScan, callback)
+		#rospy.spin()
+		talker()
+	except rospy.ROSInterruptException:
+		pass
