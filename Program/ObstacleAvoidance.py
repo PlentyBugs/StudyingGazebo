a9587fd511bc78d0498b860abb13ec32bf016dcd
PlentyBugs <43182741+PlentyBugs@users.noreply.github.com>
2019-11-16 13:46:11 +0300

    Update ObstacleAvoidance.py

--- Program/ObstacleAvoidance.py
+++ Program/ObstacleAvoidance.py
@@ -5,12 +5,13 @@ from geometry_msgs.msg import Twist
 class WrumWrumWasUberPodiehal():
 	def __init__(self):	
 		rospy.init_node('ObstacleAvoidance', anonymous=True)
-		self.sub = rospy.Subscriber('/scan', LaserScan, callback)
+		self.sub = rospy.Subscriber('/scan', LaserScan, self.callback)
 		self.cmd_vel = rospy.Publisher('/cmd_vel', Twist)
 		move_cmd = Twist()
 		rospy.on_shutdown(self.shutdown)
 		r = rospy.Rate(10);
 
+	def callback():
 		while not rospy.is_shutdown():
 
 			print('===============')
