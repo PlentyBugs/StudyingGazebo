834f1c04bfa205be6f8f58b9c83cf86879438733
PlentyBugs <43182741+PlentyBugs@users.noreply.github.com>
2019-11-16 12:28:27 +0300

    Update ObstacleAvoidance.py

--- Program/ObstacleAvoidance.py
+++ Program/ObstacleAvoidance.py
@@ -2,11 +2,13 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-def callback(msg):
+def callback(msg):	
 	print('===============')
 	print('Right: ', msg.ranges[270])
 	print('Front: ', msg.ranges[0])
 	print('Left: ', msg.ranges[90])
+
+	rospy.on_shutdown(shutdown())
 
 	if(msg.ranges[0] > 0.5):
 		move.linear.x = 0.5
@@ -16,6 +18,13 @@ def callback(msg):
 		move.angular.z = 0.0
 
 	pub.publish(move)
+
+
+def shutdown(self):
+	rospy.loginfo("Stop TurtleBot")
+	self.cmd_vel.publish(Twist())
+	rospy.sleep(1)
+
 
 rospy.init_node('ObstacleAvoidance')
 sub = rospy.Subscriber('/scan', LaserScan, callback)
